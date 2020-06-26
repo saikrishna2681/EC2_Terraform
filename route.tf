6724dbd94730ec9fe7cae958877b212f1f118eb6
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-06-26 14:58:20 +0530

    Create route.tf

---
+++ route.tf
@@ -0,0 +1,18 @@
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+
+  tags {
+    Name = "Public Subnet RT"
+  }
+}
+
+# Assign the route table to the public Subnet
+resource "aws_route_table_association" "web-public-rt" {
+  subnet_id = "${aws_subnet.public-subnet.id}"
+  route_table_id = "${aws_route_table.web-public-rt.id}"
+}
