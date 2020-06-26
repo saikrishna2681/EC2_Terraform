7f69c3aaa9a503774f28b799b13397f1efc8f2fd
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-06-26 15:26:33 +0530

    Update web_instance.tf

--- web_instance.tf
+++ web_instance.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "wb" {
    ami  = "${var.ami}"
    instance_type = "t1.micro"
-   key_name = "${aws_key_pair.default.id}"
+   key_name = "sai"
    subnet_id = "${aws_subnet.public_subnet.id}"
    vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
    associate_public_ip_address = true
