b76802eb1f57f0f6f34d5fc36cd470defe845667
SaiKrishna2681 <45065762+SaiKrishna2681@users.noreply.github.com>
2020-06-26 15:18:04 +0530

    Create web_instance.tf

---
+++ web_instance.tf
@@ -0,0 +1,16 @@
+
+# Define webserver inside the public subnet
+resource "aws_instance" "wb" {
+   ami  = "${var.ami}"
+   instance_type = "t1.micro"
+   key_name = "${aws_key_pair.default.id}"
+   subnet_id = "${aws_subnet.public_subnet.id}"
+   vpc_security_group_ids = ["${aws_security_group.sgweb.id}"]
+   associate_public_ip_address = true
+   source_dest_check = false
+   user_data = "${file("install.sh")}"
+
+  tags = {
+    Name = "webserver"
+  }
+}
